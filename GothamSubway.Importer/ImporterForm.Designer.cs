b887f0f8082af27c668f48786b85e01239d5d1c5
dlehd333 <dlehd333@gmail.com>
2020-12-15 19:16:02 +0900

    임포터 파일 로드 추가(미완)

--- GothamSubway.Importer/ImporterForm.Designer.cs
+++ GothamSubway.Importer/ImporterForm.Designer.cs
@@ -29,13 +29,109 @@ namespace GothamSubway.Importer
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.btnSaveDB = new System.Windows.Forms.Button();
+            this.rbtFootTraffic = new System.Windows.Forms.RadioButton();
+            this.rbtSatisfaction = new System.Windows.Forms.RadioButton();
+            this.rbtElectricity = new System.Windows.Forms.RadioButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(632, 425);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(689, 386);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(99, 23);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // btnSaveDB
+            // 
+            this.btnSaveDB.Location = new System.Drawing.Point(689, 415);
+            this.btnSaveDB.Name = "btnSaveDB";
+            this.btnSaveDB.Size = new System.Drawing.Size(99, 23);
+            this.btnSaveDB.TabIndex = 2;
+            this.btnSaveDB.Text = "Save Database";
+            this.btnSaveDB.UseVisualStyleBackColor = true;
+            // 
+            // rbtFootTraffic
+            // 
+            this.rbtFootTraffic.AutoSize = true;
+            this.rbtFootTraffic.Location = new System.Drawing.Point(651, 12);
+            this.rbtFootTraffic.Name = "rbtFootTraffic";
+            this.rbtFootTraffic.Size = new System.Drawing.Size(137, 16);
+            this.rbtFootTraffic.TabIndex = 3;
+            this.rbtFootTraffic.TabStop = true;
+            this.rbtFootTraffic.Text = "FootTraffic+Revenue";
+            this.rbtFootTraffic.UseVisualStyleBackColor = true;
+            // 
+            // rbtSatisfaction
+            // 
+            this.rbtSatisfaction.AutoSize = true;
+            this.rbtSatisfaction.Location = new System.Drawing.Point(651, 34);
+            this.rbtSatisfaction.Name = "rbtSatisfaction";
+            this.rbtSatisfaction.Size = new System.Drawing.Size(88, 16);
+            this.rbtSatisfaction.TabIndex = 4;
+            this.rbtSatisfaction.TabStop = true;
+            this.rbtSatisfaction.Text = "Satisfaction";
+            this.rbtSatisfaction.UseVisualStyleBackColor = true;
+            // 
+            // rbtElectricity
+            // 
+            this.rbtElectricity.AutoSize = true;
+            this.rbtElectricity.Location = new System.Drawing.Point(651, 56);
+            this.rbtElectricity.Name = "rbtElectricity";
+            this.rbtElectricity.Size = new System.Drawing.Size(78, 16);
+            this.rbtElectricity.TabIndex = 5;
+            this.rbtElectricity.TabStop = true;
+            this.rbtElectricity.Text = "Electricity";
+            this.rbtElectricity.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ImporterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rbtElectricity);
+            this.Controls.Add(this.rbtSatisfaction);
+            this.Controls.Add(this.rbtFootTraffic);
+            this.Controls.Add(this.btnSaveDB);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ImporterForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.Button btnSaveDB;
+        private System.Windows.Forms.RadioButton rbtFootTraffic;
+        private System.Windows.Forms.RadioButton rbtSatisfaction;
+        private System.Windows.Forms.RadioButton rbtElectricity;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
     }
 }
 
--- GothamSubway.Importer/ImporterForm.cs
+++ GothamSubway.Importer/ImporterForm.cs
@@ -1,20 +1,125 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace GothamSubway.Importer
 {
     public partial class ImporterForm : Form
     {
+        Excel.Workbook workbook;
+        Excel.Worksheet worksheet;
+        Excel.Application application;
+        List<string> columns;
+        List<List<string>> rows;
+
         public ImporterForm()
         {
             InitializeComponent();
+
+            workbook = null;
+            worksheet = null;
+            application = null;
+        }
+
+        private void GetExcelData(List<List<string>> rows, List<string> columns)
+        {
+            rows = new List<List<string>>();
+            columns = new List<string>();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show($"파일명 : \n{openFileDialog1.FileName}");
+
+                application = new Excel.Application();
+                workbook = application.Workbooks.Open(openFileDialog1.FileName);
+                worksheet = workbook.Sheets[1];
+                application.Visible = false;
+
+                Range range = worksheet.UsedRange;
+
+                for (int i = 1; i <= range.Rows.Count; ++i)
+                {
+                    if (i != 1)
+                        rows.Add(new List<string>());
+
+                    for (int j = 1; j <= range.Columns.Count; ++j)
+                    {
+                        if (i == 1) // 첫째줄이면?
+                        {
+                            if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                                columns.Add(((Range)range.Cells[i, j]).Value2.ToString());
+                        }
+                        else
+                        {
+                            if (range.Cells[i, j] != null && range.Cells[i, j].Value2 != null)
+                                rows[i - 2].Add(((Range)range.Cells[i, j]).Value2.ToString());
+                        }
+                    }
+                }
+
+                ReleaseObject(range);
+                ReleaseObject(worksheet);
+                ReleaseObject(workbook);
+                application.Quit();
+                ReleaseObject(application);
+            }
+        }
+
+        static void ReleaseObject(object obj)
+        {
+            try
+            {
+                if (obj != null)
+                {
+                    Marshal.ReleaseComObject(obj); // 액셀 객체 해제
+                    obj = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                throw ex;
+            }
+            finally
+            {
+                GC.Collect(); // 가비지 수집
+            }
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            GetExcelData(rows, columns);
+
+            AddGridView(dataGridView1, columns, rows);
+        }
+
+        private void AddGridView(DataGridView view, List<string> columns, List<List<string>> rows)
+        {
+            view.Rows.Clear();
+            view.RowCount = 0;
+            view.Columns.Clear();
+            view.ColumnCount = 0;
+
+            foreach (string column in columns)
+            {
+                view.ColumnCount += 1;
+                view.Columns[this.dataGridView1.Columns.Count - 1].HeaderText = column;
+                view.Columns[this.dataGridView1.Columns.Count - 1].Name = column;
+            }
+
+            foreach (List<string> row in rows)
+            {
+                view.Rows.Add(row.ToArray());
+            }
         }
     }
 }
