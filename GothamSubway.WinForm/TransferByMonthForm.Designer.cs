39d123bc85fab0f33ca1a4710af3b40bb68caea8
Daegeun_Gim <daegeungim7301@gmail.com>
2020-12-19 22:25:26 +0900

    TransferByMonthForm 최종

--- GothamSubway.WinForm/TransferByMonthForm.Designer.cs
+++ GothamSubway.WinForm/TransferByMonthForm.Designer.cs
@@ -36,20 +36,14 @@ namespace GothamSubway.WinForm
             DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
             DevExpress.XtraCharts.AreaSeriesView areaSeriesView2 = new DevExpress.XtraCharts.AreaSeriesView();
             DevExpress.XtraCharts.AreaSeriesView areaSeriesView3 = new DevExpress.XtraCharts.AreaSeriesView();
-            DevExpress.XtraLayout.ColumnDefinition columnDefinition1 = new DevExpress.XtraLayout.ColumnDefinition();
-            DevExpress.XtraLayout.ColumnDefinition columnDefinition2 = new DevExpress.XtraLayout.ColumnDefinition();
-            DevExpress.XtraLayout.RowDefinition rowDefinition1 = new DevExpress.XtraLayout.RowDefinition();
-            DevExpress.XtraLayout.RowDefinition rowDefinition2 = new DevExpress.XtraLayout.RowDefinition();
-            DevExpress.XtraLayout.RowDefinition rowDefinition3 = new DevExpress.XtraLayout.RowDefinition();
-            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
             this.footTrafficMonthlyTotalModelBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.gothamMapControl = new GothamSubway.WinForm.GothamMapControl1();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
             this.yearSelectorControl1 = new GothamSubway.WinForm.YearSelectorControl();
-            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
-            this.기준년도 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.gothamMapControl = new GothamSubway.WinForm.GothamMapControl1();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.tablePanel1 = new DevExpress.Utils.Layout.TablePanel();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            ((System.ComponentModel.ISupportInitialize)(this.footTrafficMonthlyTotalModelBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
@@ -57,18 +51,18 @@ namespace GothamSubway.WinForm
             ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(areaSeriesView2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(areaSeriesView3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.footTrafficMonthlyTotalModelBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
-            this.layoutControl1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.기준년도)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).BeginInit();
+            this.tablePanel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // footTrafficMonthlyTotalModelBindingSource
+            // 
+            this.footTrafficMonthlyTotalModelBindingSource.DataSource = typeof(GothamSubway.Data.FootTrafficMonthlyTotalModel);
             // 
             // chartControl1
             // 
-            this.chartControl1.AnimationStartMode = DevExpress.XtraCharts.ChartAnimationMode.OnLoad;
+            this.chartControl1.AnimationStartMode = DevExpress.XtraCharts.ChartAnimationMode.OnDataChanged;
+            this.chartControl1.BorderOptions.Visibility = DevExpress.Utils.DefaultBoolean.False;
             this.chartControl1.DataSource = this.footTrafficMonthlyTotalModelBindingSource;
             xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
             xyDiagram1.AxisX.WholeRange.EndSideMargin = 0D;
@@ -76,7 +70,7 @@ namespace GothamSubway.WinForm
             xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
             this.chartControl1.Diagram = xyDiagram1;
             this.chartControl1.Legend.Name = "Default Legend";
-            this.chartControl1.Location = new System.Drawing.Point(12, 68);
+            this.chartControl1.Location = new System.Drawing.Point(6, 96);
             this.chartControl1.Name = "chartControl1";
             series1.ArgumentDataMember = "date";
             series1.FilterString = "[transferId] = 1";
@@ -93,68 +87,19 @@ namespace GothamSubway.WinForm
         series2};
             areaSeriesView3.Transparency = ((byte)(0));
             this.chartControl1.SeriesTemplate.View = areaSeriesView3;
-            this.chartControl1.Size = new System.Drawing.Size(567, 446);
+            this.chartControl1.Size = new System.Drawing.Size(589, 417);
             this.chartControl1.TabIndex = 2;
             // 
-            // footTrafficMonthlyTotalModelBindingSource
+            // yearSelectorControl1
             // 
-            this.footTrafficMonthlyTotalModelBindingSource.DataSource = typeof(GothamSubway.Data.FootTrafficMonthlyTotalModel);
-            // 
-            // layoutControl1
-            // 
-            this.layoutControl1.Controls.Add(this.gothamMapControl);
-            this.layoutControl1.Controls.Add(this.chartControl1);
-            this.layoutControl1.Controls.Add(this.yearSelectorControl1);
-            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControl1.Name = "layoutControl1";
-            this.layoutControl1.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(716, 335, 650, 400);
-            this.layoutControl1.Root = this.Root;
-            this.layoutControl1.Size = new System.Drawing.Size(1022, 611);
-            this.layoutControl1.TabIndex = 3;
-            this.layoutControl1.Text = "layoutControl1";
-            // 
-            // Root
-            // 
-            this.Root.AppearanceGroup.BackColor = System.Drawing.Color.White;
-            this.Root.AppearanceGroup.Options.UseBackColor = true;
-            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
-            this.Root.GroupBordersVisible = false;
-            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem2,
-            this.layoutControlItem3,
-            this.기준년도});
-            this.Root.LayoutMode = DevExpress.XtraLayout.Utils.LayoutMode.Table;
-            this.Root.Name = "Root";
-            columnDefinition1.SizeType = System.Windows.Forms.SizeType.Percent;
-            columnDefinition1.Width = 57D;
-            columnDefinition2.SizeType = System.Windows.Forms.SizeType.Percent;
-            columnDefinition2.Width = 43D;
-            this.Root.OptionsTableLayoutGroup.ColumnDefinitions.AddRange(new DevExpress.XtraLayout.ColumnDefinition[] {
-            columnDefinition1,
-            columnDefinition2});
-            rowDefinition1.Height = 10D;
-            rowDefinition1.SizeType = System.Windows.Forms.SizeType.Percent;
-            rowDefinition2.Height = 80D;
-            rowDefinition2.SizeType = System.Windows.Forms.SizeType.Percent;
-            rowDefinition3.Height = 15D;
-            rowDefinition3.SizeType = System.Windows.Forms.SizeType.Percent;
-            this.Root.OptionsTableLayoutGroup.RowDefinitions.AddRange(new DevExpress.XtraLayout.RowDefinition[] {
-            rowDefinition1,
-            rowDefinition2,
-            rowDefinition3});
-            this.Root.Size = new System.Drawing.Size(1022, 611);
-            this.Root.TextVisible = false;
-            // 
-            // layoutControlItem2
-            // 
-            this.layoutControlItem2.Control = this.chartControl1;
-            this.layoutControlItem2.Location = new System.Drawing.Point(0, 56);
-            this.layoutControlItem2.Name = "layoutControlItem2";
-            this.layoutControlItem2.OptionsTableLayoutItem.RowIndex = 1;
-            this.layoutControlItem2.Size = new System.Drawing.Size(571, 450);
-            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem2.TextVisible = false;
+            this.tablePanel1.SetColumn(this.yearSelectorControl1, 5);
+            this.yearSelectorControl1.Location = new System.Drawing.Point(514, 549);
+            this.yearSelectorControl1.MaximumSize = new System.Drawing.Size(108, 33);
+            this.yearSelectorControl1.MinimumSize = new System.Drawing.Size(108, 33);
+            this.yearSelectorControl1.Name = "yearSelectorControl1";
+            this.tablePanel1.SetRow(this.yearSelectorControl1, 2);
+            this.yearSelectorControl1.Size = new System.Drawing.Size(108, 33);
+            this.yearSelectorControl1.TabIndex = 1;
             // 
             // gothamMapControl
             // 
@@ -162,53 +107,83 @@ namespace GothamSubway.WinForm
             this.gothamMapControl.Appearance.BackColor = System.Drawing.Color.Transparent;
             this.gothamMapControl.Appearance.Options.UseBackColor = true;
             this.gothamMapControl.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.gothamMapControl.Location = new System.Drawing.Point(583, 68);
+            this.tablePanel1.SetColumn(this.gothamMapControl, 6);
+            this.gothamMapControl.Location = new System.Drawing.Point(601, 119);
             this.gothamMapControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.gothamMapControl.MaximumSize = new System.Drawing.Size(420, 374);
             this.gothamMapControl.MinimumSize = new System.Drawing.Size(420, 374);
             this.gothamMapControl.Name = "gothamMapControl";
+            this.tablePanel1.SetRow(this.gothamMapControl, 1);
             this.gothamMapControl.Size = new System.Drawing.Size(420, 374);
             this.gothamMapControl.TabIndex = 4;
             // 
-            // yearSelectorControl1
+            // labelControl1
             // 
-            this.yearSelectorControl1.Location = new System.Drawing.Point(12, 518);
-            this.yearSelectorControl1.MaximumSize = new System.Drawing.Size(108, 33);
-            this.yearSelectorControl1.MinimumSize = new System.Drawing.Size(108, 33);
-            this.yearSelectorControl1.Name = "yearSelectorControl1";
-            this.yearSelectorControl1.Size = new System.Drawing.Size(108, 33);
-            this.yearSelectorControl1.TabIndex = 1;
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.tablePanel1.SetColumn(this.labelControl1, 4);
+            this.labelControl1.Location = new System.Drawing.Point(412, 555);
+            this.labelControl1.Name = "labelControl1";
+            this.tablePanel1.SetRow(this.labelControl1, 2);
+            this.labelControl1.Size = new System.Drawing.Size(64, 21);
+            this.labelControl1.TabIndex = 8;
+            this.labelControl1.Text = "기준년도";
             // 
-            // layoutControlItem3
+            // tablePanel1
             // 
-            this.layoutControlItem3.Control = this.gothamMapControl;
-            this.layoutControlItem3.ImageOptions.Alignment = System.Drawing.ContentAlignment.MiddleCenter;
-            this.layoutControlItem3.Location = new System.Drawing.Point(571, 56);
-            this.layoutControlItem3.Name = "layoutControlItem3";
-            this.layoutControlItem3.OptionsTableLayoutItem.ColumnIndex = 1;
-            this.layoutControlItem3.OptionsTableLayoutItem.RowIndex = 1;
-            this.layoutControlItem3.Size = new System.Drawing.Size(431, 450);
-            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem3.TextVisible = false;
+            this.tablePanel1.Appearance.BackColor = System.Drawing.Color.Transparent;
+            this.tablePanel1.Appearance.Options.UseBackColor = true;
+            this.tablePanel1.Columns.AddRange(new DevExpress.Utils.Layout.TablePanelColumn[] {
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 42.65F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 57.35F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F),
+            new DevExpress.Utils.Layout.TablePanelColumn(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 50F)});
+            this.tablePanel1.Controls.Add(this.labelControl2);
+            this.tablePanel1.Controls.Add(this.labelControl1);
+            this.tablePanel1.Controls.Add(this.gothamMapControl);
+            this.tablePanel1.Controls.Add(this.yearSelectorControl1);
+            this.tablePanel1.Controls.Add(this.chartControl1);
+            this.tablePanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tablePanel1.Location = new System.Drawing.Point(0, 0);
+            this.tablePanel1.Name = "tablePanel1";
+            this.tablePanel1.Rows.AddRange(new DevExpress.Utils.Layout.TablePanelRow[] {
+            new DevExpress.Utils.Layout.TablePanelRow(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 15F),
+            new DevExpress.Utils.Layout.TablePanelRow(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 70F),
+            new DevExpress.Utils.Layout.TablePanelRow(DevExpress.Utils.Layout.TablePanelEntityStyle.Relative, 15F)});
+            this.tablePanel1.Size = new System.Drawing.Size(1022, 611);
+            this.tablePanel1.TabIndex = 6;
             // 
-            // 기준년도
+            // labelControl2
             // 
-            this.기준년도.Control = this.yearSelectorControl1;
-            this.기준년도.Location = new System.Drawing.Point(0, 506);
-            this.기준년도.Name = "기준년도";
-            this.기준년도.OptionsTableLayoutItem.RowIndex = 2;
-            this.기준년도.Size = new System.Drawing.Size(571, 85);
-            this.기준년도.TextSize = new System.Drawing.Size(0, 0);
-            this.기준년도.TextVisible = false;
+            this.labelControl2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Malgun Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl2.Appearance.Options.UseFont = true;
+            this.tablePanel1.SetColumn(this.labelControl2, 4);
+            this.tablePanel1.SetColumnSpan(this.labelControl2, 3);
+            this.labelControl2.Location = new System.Drawing.Point(412, 23);
+            this.labelControl2.Name = "labelControl2";
+            this.tablePanel1.SetRow(this.labelControl2, 0);
+            this.labelControl2.Size = new System.Drawing.Size(289, 45);
+            this.labelControl2.TabIndex = 9;
+            this.labelControl2.Text = "월별 승하차 승객 수";
             // 
             // TransferByMonthForm
             // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1022, 611);
-            this.Controls.Add(this.layoutControl1);
+            this.Controls.Add(this.tablePanel1);
             this.Name = "TransferByMonthForm";
             this.Text = "TransferByMonthForm";
+            ((System.ComponentModel.ISupportInitialize)(this.footTrafficMonthlyTotalModelBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(areaSeriesView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
@@ -216,28 +191,22 @@ namespace GothamSubway.WinForm
             ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(areaSeriesView3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.footTrafficMonthlyTotalModelBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
-            this.layoutControl1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.기준년도)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).EndInit();
+            this.tablePanel1.ResumeLayout(false);
+            this.tablePanel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
         private System.Windows.Forms.DataGridViewTextBoxColumn monthlyTransferTotalDataGridViewTextBoxColumn;
-        private YearSelectorControl yearSelectorControl1;
         private System.Windows.Forms.BindingSource footTrafficMonthlyTotalModelBindingSource;
-        private DevExpress.XtraCharts.ChartControl chartControl1;
         private System.Windows.Forms.DataGridViewTextBoxColumn monthlyTransferTotal;
-        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private YearSelectorControl yearSelectorControl1;
+        private DevExpress.Utils.Layout.TablePanel tablePanel1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
         private GothamMapControl1 gothamMapControl;
-        private DevExpress.XtraLayout.LayoutControlGroup Root;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
-        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
-        private DevExpress.XtraLayout.LayoutControlItem 기준년도;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
     }
 }
